46bc966e28493c42454154b8afe17fbfe801cbec
nkennaA <nkenna@mit.edu>
2019-09-17 13:37:59 -0400

    making object of grocery app

--- groceryArrays/main.js
+++ groceryArrays/main.js
@@ -1,71 +1,76 @@
-var inventory = {};
-let convert = {};
-convert.fillArray = function() {
-    inventory["water"] = 100,
-    inventory["soda"] = 100,
-    inventory["detergent"] = 50,
-    inventory["apple"] = 100,
-    inventory["banana"] = 100,
-    inventory["tomato"] = 100,
-    inventory["bread"] = 50,
-    inventory["butter"] = 50,
-    inventory["milk"] = 50,
-    inventory["chicken"] = 50;
-};
-convert.fillArray();
-convert.addToInventory = function(itmesArray, numbersArray) {
-    //put the list of items you want to increase their inventory in an array, 
-    //and the number of items you wish to add in the same order
-    if (itmesArray.length != numbersArray.length) {
-        console.log("Items array and numbers array must be the same length so the numbers match the items");
-        return;
-    }
-    for(var i = 0; i < numbersArray.length; i++) {
-        if(!(itmesArray[i] in inventory)) {
-            console.log(itmesArray[i] + " ßis not an item in our inventory");
-            return;
-        } else {
-            inventory[itmesArray[i]] += numbersArray[i];
-        }
-    }
-    for(let item in inventory) {
-        console.log(item + ":" + inventory[item]);
-    }
-    return inventory;
-}
 
-convert.subtractFromInventory = function(itmesArray, numbersArray) {
-    //put the list of items you want to increase their inventory in an array, 
-    //and the number of items you wish to add in the same order
-    if (itmesArray.length != numbersArray.length) {
-        console.log("Items array and numbers array must be the same length so the numbers match the items");
-        return;
-    }
-    for(var i = 0; i < numbersArray.length; i++) {
-        if(!(itmesArray[i] in inventory)) {
-            console.log(itmesArray[i] + " is not an item in our inventory");
+var groveryObject = {
+    inventory : {},
+    fillArray : function() {
+        this.inventory["water"] = 100,
+        this.inventory["soda"] = 100,
+        this.inventory["detergent"] = 50,
+        this.inventory["apple"] = 100,
+        this.inventory["banana"] = 100,
+        this.inventory["tomato"] = 100,
+        this.inventory["bread"] = 50,
+        this.inventory["butter"] = 50,
+        this.inventory["milk"] = 50,
+        this.inventory["chicken"] = 50;
+    },
+    addToInventory : function(itmesArray, numbersArray) {
+        //put the list of items you want to increase their inventory in an array, 
+        //and the number of items you wish to add in the same order
+        if (itmesArray.length != numbersArray.length) {
+            console.log("Items array and numbers array must be the same length so the numbers match the items");
             return;
-        } else {
-            inventory[itmesArray[i]] = inventory[itmesArray[i]] - numbersArray[i];
-            if(inventory[itmesArray[i]] <= 0) {
-                var leftOvers = inventory[itmesArray[i]]*-1;
-                inventory[itmesArray[i]] = 0;
-                console.log("You could only buy " + (numbersArray[i] - leftOvers) + "\n" + itmesArray[i] + "s. " + itmesArray[i]+"s are now out of stock.");
+        }
+        for(var i = 0; i < numbersArray.length; i++) {
+            if(!(itmesArray[i] in this.inventory)) {
+                console.log(itmesArray[i] + " ßis not an item in our inventory");
+                return;
+            } else {
+                this.inventory[itmesArray[i]] += numbersArray[i];
+            }
+        }
+        for(let item in this.inventory) {
+            console.log(item + ":" + this.inventory[item]);
+        }
+        return this.inventory;
+    },
+    subtractFromInventory : function(itmesArray, numbersArray) {
+        //put the list of items you want to increase their inventory in an array, 
+        //and the number of items you wish to add in the same order
+        if (itmesArray.length != numbersArray.length) {
+            console.log("Items array and numbers array must be the same length so the numbers match the items");
+            return;
+        }
+        for(var i = 0; i < numbersArray.length; i++) {
+            if(!(itmesArray[i] in this.inventory)) {
+                console.log(itmesArray[i] + " is not an item in our inventory");
+                return;
+            } else {
+                this.inventory[itmesArray[i]] = this.inventory[itmesArray[i]] - numbersArray[i];
+                if(this.inventory[itmesArray[i]] <= 0) {
+                    var leftOvers = this.inventory[itmesArray[i]]*-1;
+                    this.inventory[itmesArray[i]] = 0;
+                    console.log("You could only buy " + (numbersArray[i] - leftOvers) + "\n" + itmesArray[i] + "s. " + itmesArray[i]+"s are now out of stock.");
+                }
+            }
+        }
+        for(let item in this.inventory) {
+            console.log(item + ":" + this.inventory[item]);
+        }
+        return this.inventory;
+    },
+    alertOutOfStock : function() {
+        for(let item in this.inventory) {
+            if(this.inventory[item] === 0) {
+                console.log(item + "s are out of stock!")
             }
         }
     }
-    for(let item in inventory) {
-        console.log(item + ":" + inventory[item]);
-    }
-    return inventory;
-}
+};
 
-function alertOutOfStock() {
-    for(let item in inventory) {
-        if(inventory[item] === 0) {
-            console.log(item + "s are out of stock!")
-        }
-    }
-}
+groveryObject.fillArray();
+
+let convert = {
+    groceries : groveryObject 
+};
 
 module.exports = convert;
--- groceryArrays/test.js
+++ groceryArrays/test.js
@@ -4,13 +4,13 @@ var assert = require('assert');
 describe('changing inventory tests', function(){
     describe('addition test', function(){
         it('should be 150 when adding 50 to apple and banana', function(){
-            var myInventory = convert.addToInventory(["apple", "banana"], [50, 50]);
+            var myInventory = convert.groceries.addToInventory(["apple", "banana"], [50, 50]);
             assert.equal(150, myInventory["apple"]);
         });
     });
     describe('subtraction test', function(){
-        convert.fillArray();
-        var myInventory2 = convert.subtractFromInventory(["water", "milk", "chicken"], [70, 70, 49]);
+        convert.groceries.fillArray();
+        var myInventory2 = convert.groceries.subtractFromInventory(["water", "milk", "chicken"], [70, 70, 49]);
         it('should be 30 when subtracting 70 from water, 0 subtracting 70 from milk, \n and 1 subtracting 49 from chicken', function(){
             assert.equal(30, myInventory2["water"]);
             assert.equal(0, myInventory2["milk"]);
