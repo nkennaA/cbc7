9ff2fae19edb665b0d5f9a3e14ee824f8edbe363
nkennaA <nkenna@mit.edu>
2019-09-18 14:17:26 -0400

    event listener project

---
+++ eventListers/main.js
@@ -0,0 +1,13 @@
+document.getElementById("id1").style.color = "blue";
+document.getElementById("id2").style.color = "green";
+document.getElementById("id3").style.color = "purple";
+document.getElementById("id3").nextElementSibling.style.color = "red";
+document.getElementById("id1").nextElementSibling.style.fontFamily = "Comic Sans,Impact,Charcoal,sans-serif";
+document.getElementById("id2").previousElementSibling.style.fontFamily = "Comic Sans,Impact,Charcoal,sans-serif";
+document.getElementById("id7").addEventListener("click", function()
+{
+    document.getElementById("id3").style.fontSize = "small";
+});
+document.getElementById("id4").addEventListener("click", function(){
+    this.style.color = "red";
+}, true);
