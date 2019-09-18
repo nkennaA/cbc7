b619c81dbf8a626e65b0177776f9aac9009274cb
nkennaA <nkenna@mit.edu>
2019-09-18 14:48:55 -0400

    changing classname

--- eventListers/main.js
+++ eventListers/main.js
@@ -2,6 +2,7 @@ document.getElementById("id1").style.color = "blue";
 document.getElementById("id2").style.color = "green";
 document.getElementById("id3").style.color = "purple";
 document.getElementById("id3").nextElementSibling.style.color = "red";
+document.getElementById("id3").nextElementSibling.className = "change";
 document.getElementById("id1").nextElementSibling.style.fontFamily = "Comic Sans,Impact,Charcoal,sans-serif";
 document.getElementById("id2").previousElementSibling.style.fontFamily = "Comic Sans,Impact,Charcoal,sans-serif";
 document.getElementById("id7").addEventListener("click", function()
